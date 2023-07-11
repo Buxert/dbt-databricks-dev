6f5b7ff22d48c12b44f708a604e892371d04e78c
Benoit Gütz <benoit.gutz@crv4all.com>
2023-07-11 11:58:18 +0200

    pre-hook added

--- dbt/include/databricks/macros/materializations/view.sql
+++ dbt/include/databricks/macros/materializations/view.sql
@@ -1,6 +1,5 @@
 {% materialization view, adapter='databricks' -%}
+    {{ run_hooks(pre_hooks) }}
     {{ return(create_or_replace_view()) }}
-
     {{ run_hooks(post_hooks) }}
-
 {%- endmaterialization %}
